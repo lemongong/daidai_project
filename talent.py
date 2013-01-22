a7495791196d5d0f345eb2dd48a1c178215deb29
Lemon <lemongong210@yahoo.com.cn>
2013-01-22 23:12:58 +0800

    Update the table create sql

--- talent.py
+++ talent.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python
-# encoding=utf-8
-#author: Wiszhou
-#date: 2013.1.20
+# -*- coding: utf-8 -*-
+#author: LemonGong
+#date: 2013-1-20
 
-'''
-人才资源管理系统， 提供人才数据录入， 修改，删除， 查询， 排序等功能。
-查询支持全字段模糊搜索。
-'''
+
+''' note: This is doc comment character.  The talent manager system,
+use to CDUT and serch fields in DB'''
 
 import MySQLdb as mysql
 
 CONN = mysql.connection(host='192.168.1.101', user='root', passwd='meego', port=3306, db='test')
 
 class Talent:
-	def __init__(self):
-		CONN.query('CREATE TABLE IF NOT EXISTS talent(id int(8), name varchar(20), PRIMARY KEY (id))')
-		return
-
-
+    def __init__(self):
+        CONN.query('CREATE TABLE IF NOT EXISTS talent(id int(8), name varchar(20), PRIMARY KEY(id),iq int(8),power int(8),charm int(8),hobby varchar(20),note varchar(30))')
+        #CONN.query('ALTER TABLE talent ADD iq int(8),ADD power int(8),ADD charm int(8),ADD hobby varchar(20),ADD note varchar(30)')
+        return
+    
 if __name__=='__main__':
-	my_talent = Talent()
+    my_talent = Talent()
